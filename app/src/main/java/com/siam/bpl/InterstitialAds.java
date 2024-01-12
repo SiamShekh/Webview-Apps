332734aaeb440277d70150f5219c73e94c2c5b80
PlanqSiam <planqnetwork@gmail.com>
2024-01-12 16:03:18 +0600

    Admob set-up

---
+++ app/src/main/java/com/siam/bpl/InterstitialAds.java
@@ -0,0 +1,44 @@
+package com.siam.bpl;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+public class InterstitialAds {
+    private InterstitialAd mInterstitialAd;
+    private Context context;
+    public InterstitialAds(Context context) {
+        this.context = context;
+
+        MobileAds.initialize(context, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(context, "ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                mInterstitialAd = interstitialAd;
+                Log.i("TAG", "onAdLoaded");
+                interstitialAd.show((Activity) context);
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/siam/bpl/Webview.java
+++ app/src/main/java/com/siam/bpl/Webview.java
@@ -6,25 +6,42 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class Webview extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     WebView web;
     ProgressBar progressBar;
     private SwipeRefreshLayout mSwipeRefreshLayout;
-
+    private AdView BannerAds;
     @SuppressLint({"MissingInflatedId", "SetJavaScriptEnabled"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
         web = findViewById(R.id.webview);
         progressBar = findViewById(R.id.progressBar);
+        BannerAds = findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        BannerAds.loadAd(adRequest);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.container);
         mSwipeRefreshLayout.setOnRefreshListener(this);
@@ -53,6 +70,27 @@ public class Webview extends AppCompatActivity implements SwipeRefreshLayout.OnR
         web.getSettings().getDefaultFontSize();
         web.loadUrl("https://fonts.google.com/");
 
+
+        adsShow1();
+    }
+
+    private void adsShow1() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                new InterstitialAds(Webview.this);
+                adsShow2();
+            }
+        }, 60000);
+    }
+    private void adsShow2() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                new InterstitialAds(Webview.this);
+                adsShow1();
+            }
+        }, 60000);
     }
 
     @Override
